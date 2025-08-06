e91a4363f6359dea734c3818baf189a3d0db337a
BHAVANA PERECHARLA <145026991+BhavanaPerecharla@users.noreply.github.com>
2025-08-06 12:19:40 +0530

    ProjectStatus updated

--- builder_portfolio_management_systems/src/main/java/com/builder_portfolio_management_systems/project/Constants/ProjectStatus.java
+++ builder_portfolio_management_systems/src/main/java/com/builder_portfolio_management_systems/project/Constants/ProjectStatus.java
@@ -1,10 +1,20 @@
 package com.builder_portfolio_management_systems.project.Constants;
 
-
-
-
+/**
+ * Enum representing the different statuses a project can have.
+ * 
+ * This is useful for maintaining type safety and avoiding the use of
+ * hard-coded string values when working with project status in the system.
+ */
 public enum ProjectStatus {
+    
+    // Indicates that the project is planned but not yet started
     UPCOMING,
+
+    // Indicates that the project is currently in progress
     IN_PROGRESS,
+
+    // Indicates that the project has been finished
     COMPLETED
 }
+
