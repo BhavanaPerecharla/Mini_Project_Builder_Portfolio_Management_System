85fef36391528695d6f3b2eeef4049341baa133f
BHAVANA PERECHARLA <145026991+BhavanaPerecharla@users.noreply.github.com>
2025-08-06 12:54:41 +0530

    Updated UserService

--- builder_portfolio_management_systems/src/main/java/com/builder_portfolio_management_systems/project/Service/UserService.java
+++ builder_portfolio_management_systems/src/main/java/com/builder_portfolio_management_systems/project/Service/UserService.java
@@ -1,20 +1,37 @@
 package com.builder_portfolio_management_systems.project.Service;
 
 
-
-
 import java.util.List;
 
 import com.builder_portfolio_management_systems.project.DTO.UserRequest;
 import com.builder_portfolio_management_systems.project.DTO.UserResponse;
 
+/**
+ * UserService interface provides operations related to user management.
+ * It abstracts business logic for creating, retrieving, and managing user data.
+ */
 public interface UserService {
 
+    /**
+     * Creates a new user from the given request data.
+     *
+     * @param request UserRequest object containing user details (name, email, password, role).
+     * @return UserResponse representing the created user.
+     */
     UserResponse createUser(UserRequest request);
 
+    /**
+     * Retrieves a list of all users in the system.
+     *
+     * @return List of UserResponse objects representing all users.
+     */
     List<UserResponse> getAllUsers();
 
+    /**
+     * Retrieves a user by their unique ID.
+     *
+     * @param id Long - Unique identifier of the user.
+     * @return UserResponse representing the user with the given ID.
+     */
     UserResponse getUserById(Long id);
-
-
 }
